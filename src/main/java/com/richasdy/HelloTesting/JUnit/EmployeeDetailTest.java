5262e66ab75e79f91a1214190211acfc1056ae61
Donni Richasdy <donni.richasdy@gmail.com>
2016-10-05 11:09:00 +0700

    creating test case

---
+++ src/main/java/com/richasdy/HelloTesting/JUnit/EmployeeDetailTest.java
@@ -0,0 +1,32 @@
+package com.richasdy.HelloTesting.JUnit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class EmployeeDetailTest {
+	EmployeeBusinessLogic empBusinessLogic = new EmployeeBusinessLogic();
+	EmployeeDetails employee = new EmployeeDetails();
+
+	// test to check appraisal
+	@Test
+	public void testCalculateAppriasal() {
+		employee.setName("Rajeev");
+		employee.setAge(25);
+		employee.setMonthlySalary(8000);
+
+		double appraisal = empBusinessLogic.calculateAppraisal(employee);
+		assertEquals(500, appraisal, 0.0);
+	}
+
+	// test to check yearly salary
+	@Test
+	public void testCalculateYearlySalary() {
+		employee.setName("Rajeev");
+		employee.setAge(25);
+		employee.setMonthlySalary(8000);
+
+		double salary = empBusinessLogic.calculateYearlySalary(employee);
+		assertEquals(96000, salary, 0.0);
+	}
+}
---
+++ src/main/java/com/richasdy/HelloTesting/JUnit/EmployeeTestRunner.java
@@ -0,0 +1,17 @@
+package com.richasdy.HelloTesting.JUnit;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class EmployeeTestRunner {
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(EmployeeDetailTest.class);
+
+		for (Failure failure : result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+
+		System.out.println(result.wasSuccessful());
+	}
+}
